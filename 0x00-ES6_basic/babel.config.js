98ac98b7210c3d5c0b281d54bc7b052372f81f5b
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-09-25 23:39:26 +0300

    Update babel.config.js

--- 0x00-ES6_basic/babel.config.js
+++ 0x00-ES6_basic/babel.config.js
@@ -1,9 +1,12 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', {
+const presets = [
+  [
+    '@babel/preset-env',
+    {
       targets: {
-        node: 'current',
-      },
-    }],
-  ],
-};
+        node: 'current'
+      }
+    }
+  ]
+]
+
+module.exports = { presets }
