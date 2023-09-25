0afcebaad80de161c17abc6a9bc165f8c17fe980
solamsalu <solamsalu@gmail.com>
2023-09-25 19:38:11 +0000

    update

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,32 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "plugin:jest/all"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["jest"],
+  rules: {
+    "no-console": "off",
+    "no-shadow": "off",
+    "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      excludedFiles: "babel.config.js",
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+  ],
+};
