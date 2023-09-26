98a27cedda005861b8576b8849a2270257835270
solamsalu <solamsalu@gmail.com>
2023-09-26 19:11:21 +0000

    Task 0

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        // Your API call logic goes here
+        // If the API call is successful, call resolve(data)
+        // If the API call fails, call reject(error)
+    });
+}
