e83a1cdb0c866006df7c1869c804f1eabf1144d1
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-09-29 23:54:48 +0300

    Task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,39 +1,39 @@
 export default class Currency {
-    constructor(code, name) {
-	if (typeof code !== 'string') {
-	    throw TypeError('Code must be a string');
-	}
-	if (typeof name !== 'string') {
-	    throw TypeError('Name must be a string');
-	}
-
-	this._code = code;
-	this._name = name;
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
     }
 
-    get code() {
-	return this._code;
-    }
+    this._code = code;
+    this._name = name;
+  }
 
-    set code(newCode) {
-	if (typeof newCode !== 'string') {
-	    throw TypeError('Code must be a string');
-	}
-	this._code = newCode;
-    }
+  get code() {
+    return this._code;
+  }
 
-    get name() {
-	return this._name;
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw TypeError('Code must be a string');
     }
+    this._code = newCode;
+  }
 
-    set name(newName) {
-	if (typeof newName !== 'string') {
-	    throw TypeError('Name must be a string');
-	}
-	this._name = newName;
-    }
+  get name() {
+    return this._name;
+  }
 
-    displayFullCurrency() {
-	return `${this._name} (${this._code})`;
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
     }
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
