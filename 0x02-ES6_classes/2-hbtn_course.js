6de6fb1ae9c14c465f409c2917da42b0991ba2e9
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-09-29 23:53:41 +0300

    Task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,4 +1,4 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
       throw TypeError('Name must be a string');
