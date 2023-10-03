b1f78679d2d8f064137e5bd835ead7cced5cf710
Solomon Amsalu <solamsalu@gmail.com>
2023-10-03 15:42:13 +0300

    eslint fixed

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(arr) {
-   if (!Array.isArray(arr)) {
-      return [];
-   }
+  if (!Array.isArray(arr)) {
+    return [];
+  }
 
-   return arr.map(obj => obj.id);
+  return arr.map((obj) => obj.id);
 }
