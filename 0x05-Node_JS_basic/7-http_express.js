33992e5eeb67fe67d57e35515acbdc062d6e9aeb
Solomon Amsalu <solamsalu@gmail.com>
2023-11-21 22:46:11 +0300

    Task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,13 +1,27 @@
 const express = require('express');
 
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
+
 const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(1245, '0.0.0.0', () => {
-  console.log('Listening on port 1245');
+app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
+  }
 });
+
+app.listen(port, () => { });
 
 module.exports = app;
