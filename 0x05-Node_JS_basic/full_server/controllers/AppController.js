044b81c90a1abc17826b12d2bdc642079b1fc8a6
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-11-21 23:46:30 +0300

    Update AppController

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,7 +1,7 @@
 class AppController {
-   static getHomepage(request, response) {
-     response.send(200, 'Hello Holberton School!');
-   }
- }
- 
- export default AppController;
+  static getHomepage(request, response) {
+    response.send(200, 'Hello Holberton School!');
+  }
+}
+
+export default AppController;
