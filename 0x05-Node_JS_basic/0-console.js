35b7039cc3f0968eb21085111b5f1283ddd06965
Solomon Amsalu <solamsalu@gmail.com>
2023-11-21 17:37:14 +0300

    Task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,16 +1,5 @@
-/**
- * Logs a message to the console.
- * @param {string} message - The message to log.
- * @returns {void}
- */
-// Arrow function
-const displayMessage = (message) => console.log(message);
-
-/**
- * Exports the displayMessage function.
- * @module displayMessage
- * @function
- */
-// Default module export.
-module.exports = displayMessage;
-
+const displayMessage = (message) => {
+   console.log(message);
+ };
+ 
+ module.exports = displayMessage;
