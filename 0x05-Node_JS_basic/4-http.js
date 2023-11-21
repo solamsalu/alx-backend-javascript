5540b9813ab0e1a83b5bf2869486d9fb792700e5
Solomon Amsalu <solamsalu@gmail.com>
2023-11-21 22:23:04 +0300

    Task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const { createServer } = require('http');
 const PORT = 1245;
 const HOST = '127.0.0.1';
 
-const app = createServer((req, resp) => {
+const app = createServer((_req, resp) => {
   resp.statusCode = 200;
   resp.setHeader('Content-Type', 'text/plain');
   resp.end('Hello Holberton School!');
