192e84d65ba7c4599a6b087ce5bc48be13a61f53
Solomon Amsalu <solamsalu@gmail.com>
2023-11-21 17:14:25 +0300

    Task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,9 @@
+/**
+ * Displays the provided message on the standard output.
+ * @param {string} message - The message to be displayed.
+ */
 function displayMessage(message) {
    console.log(message);
  }
  
+ module.exports = displayMessage;
