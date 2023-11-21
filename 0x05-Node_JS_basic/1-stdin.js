721e3c923cd35597b72f5ff282e578ae646aaada
Solomon Amsalu <solamsalu@gmail.com>
2023-11-21 17:54:26 +0300

    Task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data !== null) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
