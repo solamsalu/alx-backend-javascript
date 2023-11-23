348de2ef767cfc891d3a6f91107620090eb1b273
Solomon Amsalu <solamsalu@gmail.com>
2023-11-23 16:05:09 +0300

    Task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,17 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', () => {
-  it('should round and sum two numbers', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4)
-  });
-  it('should round and sum two numbers', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5)
-  });
-  it('should round and sum two numbers', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5)
-  });
-  it('should round and sum two numbers', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
+describe('calculateNumber', function () {
+   it('test #1', function () {
+      assert.equal(calculateNumber(1, 3), 4);
+   });
+   it('test #2', function () {
+      assert.equal(calculateNumber(1, 3.7), 5);
+   });
+   it('test #3', function () {
+      assert.equal(calculateNumber(1.2, 3.7), 5);
+   });
+   it('test #4', function () {
+      assert.equal(calculateNumber(1.5, 3.7), 6);
+   });
 });
